548eeec4c72b12a34f83d7e6977c21b46398b8cd
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-09 00:40:30 +0000

    Chose order of ID's

--- db/migrate/20180409003559_create_join_table_session_student.rb
+++ db/migrate/20180409003559_create_join_table_session_student.rb
@@ -1,8 +1,7 @@
 class CreateJoinTableSessionStudent < ActiveRecord::Migration[5.1]
   def change
     create_join_table :sessions, :students do |t|
-      # t.index [:session_id, :student_id]
-      # t.index [:student_id, :session_id]
+      t.index [:session_id, :student_id]
     end
   end
 end
