2117f0ae048d39dce153d1c3be0fc0ed3d1530b5
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-09 00:05:37 +0000

    Run Student migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180408235747) do
+ActiveRecord::Schema.define(version: 20180409000227) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tutors", force: :cascade do |t|
     t.string "name"
