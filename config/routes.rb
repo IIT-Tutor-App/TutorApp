9e7480d1de03a38b8e8a7692fcb4459d86007f91
Katie Ediger <katieediger@gmail.com>
2018-04-11 18:03:08 -0500

    Updated routes.rb to match Google OAuth callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/redirect'
+  match 'auth/:provider/callback', to: 'sessions#redirect', via: [:post,:get]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
