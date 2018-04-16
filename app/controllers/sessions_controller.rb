e25652037b77aa94cd5ba24b9672d186931b37a8
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-16 23:28:35 +0000

    Fixed typos

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
     provider_response = request.env['omniauth.auth']
     student =  Student.new
     student.name = provider_response['info']['name']
-    studente.email = provider_response['info']['email']
+    student.email = provider_response['info']['email']
     if student.save
-      redirect_to search_show_path
+      redirect_to search_path
     else
       ### Error handeling
+    end
   end
 end
