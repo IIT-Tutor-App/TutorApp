e1ac516b84ca4eb56a0f2f62ce91f5130f580f9b
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-16 23:21:41 +0000

    Deleted redirect view and added code to create Student from google oauth hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   def redirect
-    @provider_response = request.env['omniauth.auth']
+    provider_response = request.env['omniauth.auth']
+    student =  Student.new
+    student.name = provider_response['info']['name']
+    studente.email = provider_response['info']['email']
+    if student.save
+      redirect_to search_show_path
+    else
+      ### Error handeling
   end
 end
