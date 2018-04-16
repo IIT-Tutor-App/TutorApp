3b04a40a9a57f1bb29b68aa37ee1f18191a6676e
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-16 23:22:55 +0000

    Generated search controller
    	$ rails g controller search show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'search/show'
+
   root 'landing#show'
   match 'auth/:provider/callback', to: 'sessions#redirect', via: [:post,:get]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
