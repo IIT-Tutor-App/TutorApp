5f925eba6710cd581e8baa479901d32def4b9841
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-16 23:30:20 +0000

    Changed route names for /search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'search/show'
-
   root 'landing#show'
+  
+  get 'search', to: 'search#show', as: 'search'
+  
   match 'auth/:provider/callback', to: 'sessions#redirect', via: [:post,:get]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
