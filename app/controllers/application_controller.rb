f4d2416040eae224124f426d8a6222638e6546b9
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-17 05:59:01 +0000

    Wrote helper methods for user login status, creating session for new or old user and getting current user data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :signed_in?
+
+  private
+
+  def current_user
+    @current_user ||= Student.find_by(email: session[:user_email])
+  end
+  
+  def signed_in?
+    !!current_user
+  end
+  
+  def current_user=(user)
+    session[:user_name] = user.name
+    session[:user_email] = user.email
+  end
 end
