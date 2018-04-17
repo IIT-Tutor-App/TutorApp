68e4e1bbac69b2acbf5bfe7da354c31312340f4b
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-17 05:58:00 +0000

    Added route for logout action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   get 'search', to: 'search#show', as: 'search'
   
+  get 'logout', to: 'sessions#logout', as: 'logout'
+  
   match 'auth/:provider/callback', to: 'sessions#redirect', via: [:post,:get]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
