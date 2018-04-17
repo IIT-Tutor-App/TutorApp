dbcee0fb7ad142fb2d563ca0a82ce30143e56ee7
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-17 06:19:17 +0000

    Added code to seed fake data into the tutors table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# I'm using faker to generate fake data for Tutors. You can do the same to create random sessions by looking at this https://github.com/stympy/faker/blob/master/doc/date.md
+for x in 1..10
+  tutor = Tutor.new
+  tutor.name = Faker::Name.name
+  tutor.email = Faker::Internet.email
+  tutor.bio = Faker::Lorem.paragraphs(1)[0]
+  tutor.subject = Faker::Pokemon.name
+  tutor.save
+end
