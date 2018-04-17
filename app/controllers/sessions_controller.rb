31b2664c9b40df8385ee6273d58732106de5c403
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-17 05:59:50 +0000

    Changed redirect action to accomodate for already existing user and added logout action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,21 @@ class SessionsController < ApplicationController
     student =  Student.new
     student.name = provider_response['info']['name']
     student.email = provider_response['info']['email']
-    if student.save
+    if Student.where(email: student.email).exists?
+      self.current_user = student
       redirect_to search_path
     else
-      ### Error handeling
+      if student.save
+        self.current_user = student
+        redirect_to search_path
+      else
+        ### Error handeling
+      end
     end
+  end
+  
+  def logout
+    reset_session
+    redirect_to root_url
   end
 end
